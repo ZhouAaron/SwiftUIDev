aaa12c6451895e3b6a2c447790d3e04916e1c298
aaron_Zhou1 <zj971021@163.com>
2022-02-21 18:34:01 +0800

    Initial Commit

---
+++ design code 15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  design code 15
+//
+//  Created by Aaron on 2022/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ design code 15/design_code_15App.swift
@@ -0,0 +1,17 @@
+//
+//  design_code_15App.swift
+//  design code 15
+//
+//  Created by Aaron on 2022/2/21.
+//
+
+import SwiftUI
+
+@main
+struct design_code_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
