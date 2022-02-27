d6ba72b2f7ec0ab4a5a3553b487a0e8973a94c69
aaron_Zhou1 <zj971021@163.com>
2022-02-27 23:23:06 +0800

    section 30

---
+++ design code 15/Components/CourseItem.swift
@@ -0,0 +1,20 @@
+//
+//  CourseItem.swift
+//  design code 15
+//
+//  Created by Aaron on 2022/2/27.
+//
+
+import SwiftUI
+
+struct CourseItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseItem()
+    }
+}
